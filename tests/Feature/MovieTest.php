77a20b6682d0d141223324b88914f8cdc9e8f6b6
Federico Boncaldo <federico@cryptolab.net>
2021-06-03 19:54:50 +0100

    Import Movie model in MovieTest

--- tests/Feature/MovieTest.php
+++ tests/Feature/MovieTest.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Tests\Feature;
+namespace App\tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Movie;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class MovieTest extends TestCase
 {
