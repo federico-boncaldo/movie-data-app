97c228c061314a84e680c06f05ecda13642d4a62
Federico Boncaldo <federico@cryptolab.net>
2021-06-04 13:46:58 +0100

    Adjust layout of movie list component

--- resources/js/components/MovieList.vue
+++ resources/js/components/MovieList.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="container">
-		<h1>Movie List</h1>
-		<div class="row">
-			<div class="col-md-6">
-				<button @click="searchMovie('Matrix')">Search Matrix</button>
-				<button @click="searchMovie('Matrix Reloaded')">Search Matrix Reloaded</button>
-				<button @click="searchMovie('Matrix Revolutions')">Search Matrix Revolutions</button>
-			</div>
-			<div class="col-md-6">
-				<div v-for="movie in movies" :key="movie.id">
+		<h1 class="p-2 text-center">Movie List</h1>
+		<div class="text-center">
+			<button type="button" class="btn btn-primary" @click="searchMovie('Matrix')">Search Matrix</button>
+			<button type="button" class="btn btn-primary" @click="searchMovie('Matrix Reloaded')">Search Matrix Reloaded</button>
+			<button type="button" class="btn btn-primary" @click="searchMovie('Matrix Revolutions')">Search Matrix Revolutions</button>
+		</div>
+		<div>
+			<div v-for="movie in movies" :key="movie.id" class="card m-2">
+				<div class="card-body">
 					<p>Title: {{ movie.Title }}</p>
 					<p>Year: {{ movie.Year }}</p>
 					<p>Category:{{ movie.Type }}</p>
