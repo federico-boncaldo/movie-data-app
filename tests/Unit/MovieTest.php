00ce95e5b88efd47dd19b0966c0160fc289a8a8d
Federico Boncaldo <federico@cryptolab.net>
2021-06-04 15:19:55 +0100

    Adjust data for has_a_poster test

--- tests/Unit/MovieTest.php
+++ tests/Unit/MovieTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\Movie;
+use App\Models\Poster;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -21,14 +22,17 @@ class MovieTest extends TestCase
         Movie::factory()->count(2)->sameImdbID()->create();
     }
 
+    /** @test */
     public function has_a_poster()
     {
         $movie = Movie::factory()->create();
         $poster = new Poster();
         $poster->url = $this->faker->imageUrl('');
+        $poster->save();
 
-        $movie->poster();
+        $movie->poster()->associate($poster);
+        $movie->save();
 
-        $this->assertEquals($movie->poster()->url, $poster->url);
+        $this->assertEquals($movie->poster->url, $poster->url);
     }
 }
