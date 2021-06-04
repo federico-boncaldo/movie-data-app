282d61b38f0bd5e2bb8d14d55e59b656bcae2ef8
Federico Boncaldo <federico@cryptolab.net>
2021-06-04 15:23:02 +0100

    Create movie poster relationship

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'year', 'imdb_id', 'type'];
+
+    public function poster()
+    {
+        return $this->belongsTo(Poster::class);
+    }
 }
