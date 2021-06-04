247010e00672094517946df70769e60cd9c31da1
Federico Boncaldo <federico@cryptolab.net>
2021-06-04 13:07:37 +0100

    Add MovieList.vue to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+
+Vue.component('movie-list', require('./components/MovieList.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+});
