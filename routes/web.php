7a934a6bf51ee66bf46583682c069479958f2790
Federico Boncaldo <federico@cryptolab.net>
2021-06-04 08:38:38 +0100

    Use MoviesController for movies route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/movies/', function () {
-    return view('welcome');
-});
+Route::get('/movies', [MoviesController::class, 'index']);
